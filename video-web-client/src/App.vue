3d1760b4133e7969e2d3e78d569f731fadaf7569
tsmialek <109021396+tsmialek@users.noreply.github.com>
2025-04-23 22:10:36 +0200

    Create Vue starter project (#5)

---
+++ video-web-client/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header><h1>Hello world!</h1></header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
