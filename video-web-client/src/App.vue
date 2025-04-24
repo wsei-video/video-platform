b67a3b920b54a37ee870eb9d36855b18a4eb6b3e
Oskar <oskar.smilek@microsoft.wsei.edu.pl>
2025-04-24 21:25:08 +0200

    Add basic Bootstrap configuration

--- video-web-client/src/App.vue
+++ video-web-client/src/App.vue
@@ -1,11 +1,61 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+
+const variants = ['primary', 'secondary', 'success', 'info', 'warning', 'danger', 'light', 'dark']
 </script>
 
 <template>
-  <header><h1>Hello world!</h1></header>
+  <div class="container my-3">
+    <h1>Heading 1</h1>
+    <h2>Heading 2</h2>
+    <h3>Heading 3</h3>
+    <h4>Heading 4</h4>
+    <h5>Heading 5</h5>
+    <h6>Heading 6</h6>
 
-  <RouterView />
+    <div v-for="variant in variants" class="pb-2" :key="variant">
+      <button :class="['btn', `btn-${variant}`]">Button {{ variant }}</button>
+    </div>
+
+    <div class="card">
+      <div class="card-header">
+        <h3>Card header</h3>
+      </div>
+      <div class="card-body">Card content</div>
+      <div class="card-footer">Card footer</div>
+    </div>
+
+    <form>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input
+          type="email"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+        />
+        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input
+          type="password"
+          placeholder="password"
+          class="form-control"
+          id="exampleInputPassword1"
+        />
+      </div>
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+
+  <div class="container">
+    <RouterView />
+  </div>
 </template>
 
 <style scoped></style>
