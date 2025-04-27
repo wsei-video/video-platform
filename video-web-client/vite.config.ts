45f09446a13f36af244559bc2da42840d2185a8a
Oskar <115578833+oskar-wsei@users.noreply.github.com>
2025-04-27 16:19:59 +0200

    Add basic Bootstrap configuration (#9)

--- video-web-client/vite.config.ts
+++ video-web-client/vite.config.ts
@@ -15,4 +15,12 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'legacy',
+        quietDeps: true
+      },
+    },
+  },
 })
