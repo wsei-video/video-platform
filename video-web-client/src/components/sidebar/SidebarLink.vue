690ebc6c4ca6929dbd4b6500f09b3ade4a90a822
tsmialek <109021396+tsmialek@users.noreply.github.com>
2025-06-14 09:46:07 +0200

    Add sidebar and sidebar link components (#20)

---
+++ video-web-client/src/components/sidebar/SidebarLink.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import AppIcon from '../ui/AppIcon.vue'
+
+defineProps<{ icon?: string; to: string }>()
+</script>
+
+<template>
+  <RouterLink :to="to" custom v-slot="{ navigate, href }" class="text-decoration-none text-white">
+    <a @click="navigate" :href="href" class="fw-bold sidebar-link">
+      <AppIcon v-if="icon" :name="icon" class="sidebar-link-icon" />
+      <span class="sidebar-link-description"><slot /></span>
+    </a>
+  </RouterLink>
+</template>
+
+<style lang="scss">
+@import '../../styles/bootstrap/index.scss';
+
+.sidebar-link {
+  display: flex;
+  text-decoration: none;
+  gap: 1rem;
+  align-items: center;
+  padding: 0.625rem;
+  width: 100%;
+  border-radius: 8px;
+  color: $inactive-link;
+
+  &:hover,
+  &.selected {
+    background-color: $accent;
+    color: white;
+    cursor: pointer;
+  }
+
+  .sidebar-link-icon {
+    font-size: 2rem;
+    flex-shrink: 0;
+  }
+
+  .sidebar-link-description {
+    flex-grow: 1;
+  }
+}
+</style>
